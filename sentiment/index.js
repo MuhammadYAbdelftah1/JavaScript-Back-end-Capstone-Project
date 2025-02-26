1debfd0b63cba9140aaae2e1e7292135c4bbb3e5
Muhammad Youssef <muhammadyoussef@Muhammads-Laptop.local>
2025-02-26 03:34:35 +0200

    Import natural npm package in sentiment/index.js

---
+++ sentiment/index.js
@@ -0,0 +1,5 @@
+const natural = require('natural');
+
+// Example usage of the natural package
+const tokenizer = new natural.WordTokenizer();
+console.log(tokenizer.tokenize("Your Node.js sentiment analysis project is ready!"));
